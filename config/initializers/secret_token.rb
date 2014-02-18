669e749c9aabc3b02b6730f0b708d9e17c0e029d
Programators <programtorseu@gmail.com>
2014-02-19 00:01:49 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pintube::Application.config.secret_key_base = '6627c1c70a506a8faeb099064e56fb59e7fb47bcc0a201da3fc6104bf1a85a3675bf56b284f739501a26404fabd05e6cbc5be98f3ea96131f6c10adacd967a3e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pintube::Application.config.session_store :cookie_store, key: '_pintube_session'
